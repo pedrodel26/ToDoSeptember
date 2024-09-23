6f3fafccbc08da07567916c07086614e121681eb
pedrodel26 <pedromodesto26@gmail.com>
2024-09-23 15:34:27 -0300

    Initial Commit

---
+++ ToDoSeptember/ToDoSeptemberApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSeptemberApp.swift
+//  ToDoSeptember
+//
+//  Created by Pedro Delmondes  on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSeptemberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
